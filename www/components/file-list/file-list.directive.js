80c805bbf1661701a70540effcd8d946764e34a4
thiago <tersariol19@gmail.com>
2017-06-22 21:41:11 -0300

    correcao bug upload imagens

--- www/components/file-list/file-list.directive.js
+++ www/components/file-list/file-list.directive.js
@@ -11,20 +11,22 @@
                 },
                 link: function(scope, el, attrs){
 
-                    el.bind('change', function(event){
+                    el.on('change', function(event){
                         var files = event.target.files;
                         var file = files[0];
                         if(file && typeof(file) !== undefined && file.size > 0){
                             scope.file = file;
                             scope.$parent.file = file;
                             FileListUtil.set(file);
+                            event.currentTarget.value = "";
                         }
                         else {
                             scope.file = {};
                             scope.$parent.file = {};
                             FileListUtil.set(file);
+                            event.currentTarget.value = "";
                         }
-                        scope.$apply();
+                        scope.$apply(); 
                     });
 
 
