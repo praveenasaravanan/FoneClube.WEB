4126c75d914558d210ea83c47770736c5597213a
paramby <info@cybrogen.com>
2018-02-10 01:58:02 +0530

    search in all fields fix

--- www/modules/customers/customers.controller.js
+++ www/modules/customers/customers.controller.js
@@ -39,11 +39,69 @@
             //alert(item.Name);     
             if (!vm.search)
                 return true;       
-            var text = removeAccents(item.Name.toLowerCase());
+            //var text = removeAccents(item.Name.toLowerCase());
             //alert(text);
-            var search = removeAccents(vm.search.toLowerCase()).replace(/[!#$%&'()*+,-./:;?@[\\\]_`{|}~]/g, '');
-            return text.indexOf(search) > -1;
+            //var search = removeAccents(vm.search.toLowerCase()).replace(/[!#$%&'()*+,-./:;?@[\\\]_`{|}~]/g, '');
+            //return text.indexOf(search) > -1;
+            var objects = [];
+            var jsonstr=JSON.stringify(item);
+            var parsejson=JSON.parse(jsonstr);
+            var searchterm=vm.search.replace(/[!#$%&'()*+,-./:;?@[\\\]_`{|}~]/g, '');
+            objects=getKeys(parsejson,searchterm);
+            return objects.length > 0;
         };
+
+                //return an array of keys that match on a certain value
+                function getKeys(obj, val) {
+                    var objects = [];
+                    for (var i in obj) {
+                        if (!obj.hasOwnProperty(i)) continue;
+                        if (typeof obj[i] == 'object') {
+                            objects = objects.concat(getKeys(obj[i], val));
+                        } else if (obj[i] != null) {
+                            //console.log(obj[i].toString().toLowerCase());
+                            if (removeAccents(obj[i].toString().toLowerCase()).indexOf(removeAccents(val.toLowerCase())) > -1) {
+                            objects.push(i);
+                            }
+                        }
+                    }
+                    return objects;
+                }
+
+                //return an array of values that match on a certain key
+                function getValues(obj, key) {
+                    var objects = [];
+                    for (var i in obj) {
+                        if (!obj.hasOwnProperty(i)) continue;
+                        if (typeof obj[i] == 'object') {
+                            objects = objects.concat(getValues(obj[i], key));
+                        } else if (i == key) {
+                            objects.push(obj[i]);
+                        }
+                    }
+                    return objects;
+                }        
+
+        //return an array of objects according to key, value, or key and value matching
+        function getObjects(obj, key, val) {
+            var objects = [];
+            for (var i in obj) {
+                if (!obj.hasOwnProperty(i)) continue;
+                if (typeof obj[i] == 'object') {
+                    objects = objects.concat(getObjects(obj[i], key, val));
+                } else
+                    //if key matches and value matches or if key matches and value is not passed (eliminating the case where key matches but passed value does not)
+                    if (i == key && obj[i] == val || i == key && val == '') { //
+                        objects.push(obj);
+                    } else if (obj[i] == val && key == '') {
+                        //only add if the object is not already in the array
+                        if (objects.lastIndexOf(obj) == -1) {
+                            objects.push(obj);
+                        }
+                    }
+            }
+            return objects;
+        }
 
         var getCustomers = $scope.$watch(function() {
             return vm.data.customers;
