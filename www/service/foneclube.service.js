effb9b6326c6b313eea1a3988cbc76377ad95afd
Rodrigo Cardozo <rodrigocardozop@gmail.com>
2018-06-11 21:22:57 -0300

    homol

--- www/service/foneclube.service.js
+++ www/service/foneclube.service.js
@@ -9,10 +9,10 @@
     function FoneclubeService($q, HTTPService) {
 
       /// var urlApi = 'http://webapidemo.devworkdemo.com/api';
-        var urlApi = 'http://localhost:57078/api';
+        // var urlApi = 'http://localhost:57078/api';
 
         //API HOMOL dispatchedCommision
-        // var urlApi = 'http://homol-api.p2badpmtjj.us-east-2.elasticbeanstalk.com/api';
+        var urlApi = 'http://homol-api.p2badpmtjj.us-east-2.elasticbeanstalk.com/api';
 
         //API QUE VAI SER PROD
        // var urlApi = 'http://default-environment.p2badpmtjj.us-east-2.elasticbeanstalk.com/api'
