243c284d3aefd4297a3850b3466e5400bbbea38c
Rodrigo Cardozo <rodrigocardozop@gmail.com>
2018-06-14 21:50:18 -0300

    andamento

--- www/service/foneclube.service.js
+++ www/service/foneclube.service.js
@@ -9,10 +9,10 @@
     function FoneclubeService($q, HTTPService) {
 
         
-        var urlApi = 'http://localhost:57078/api';
+        // var urlApi = 'http://localhost:57078/api';
 
         //API tests 
-        //var urlApi = 'http://homol-api.p2badpmtjj.us-east-2.elasticbeanstalk.com/api';
+        var urlApi = 'http://homol-api.p2badpmtjj.us-east-2.elasticbeanstalk.com/api';
 
         //API live
         //var urlApi = 'http://default-environment.p2badpmtjj.us-east-2.elasticbeanstalk.com/api'
