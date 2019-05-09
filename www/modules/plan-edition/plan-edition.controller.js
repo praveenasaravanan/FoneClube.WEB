e7edf2661c23e8d7b48645625c8d5315ba7f92f8
Rodrigo Cardozo <rodrigocardozop@gmail.com>
2019-05-08 21:17:40 -0300

    andamento

--- www/modules/plan-edition/plan-edition.controller.js
+++ www/modules/plan-edition/plan-edition.controller.js
@@ -9,6 +9,7 @@
       function PlanEditionController(FlowManagerService, FoneclubeService, PagarmeService) {
             var vm = this;
             vm.ativacao = [true,false];
+            vm.planOperator = ['CLARO','VIVO']
             vm.onClickEditPlan = onClickEditPlan;
             vm.onClickEditService = onClickEditService;
             // vm.operator = [{id:1, operator:'CLARO'},{id:2, operator:'VIVO'}]
@@ -21,9 +22,9 @@
                 vm.allPlans = result;
                 for(var i in vm.allPlans){
                     if(vm.allPlans[i].IdOperator == 1)
-                        vm.allPlans[i].currentPlan = [{id:1, operator:'CLARO', selected:true},{id:2, operator:'VIVO'}]
+                        vm.allPlans[i].currentPlan = 'CLARO'
                     else
-                        vm.allPlans[i].currentPlan = [{id:2, operator:'VIVO', selected:true}, {id:1, operator:'CLARO'}]
+                        vm.allPlans[i].currentPlan = 'VIVO'
                     
                 }
 
@@ -53,18 +54,28 @@
                 FoneclubeService.postUpdateServiceFoneclube(service).then(function (result) {
                     console.log('result services postUpdateServiceFoneclube')
                     console.log(result)
-                    
+                    if(result)
+                        alert('Serviço atualizado')
+                    else{
+                        alert('Serviço não atualizado')
+                    }    
                 })
             }
 
             function onClickEditPlan(plan){
                 console.log('edit plan:')
                 console.log(plan)
-
+                plan.Active = plan.selectedActive;
+                console.log(vm.selectedOperatorId)
                 debugger
                 FoneclubeService.postUpdatePhonePlan(plan).then(function (result) {
                     console.log('result services postUpdatePhonePlan')
                     console.log(result)
+                    if(result)
+                        alert('Plano atualizado')
+                    else{
+                        alert('Plano não atualizado')
+                    } 
                     
                 })
             }
