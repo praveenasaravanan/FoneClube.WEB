1223ec62dcb1000034038da8eab584bd2fc5a3cd
Rodrigo Cardozo <rodrigocardozop@gmail.com>
2019-09-03 21:26:37 -0300

    andamento flags e envio de email

--- www/components/main/main.components.js
+++ www/components/main/main.components.js
@@ -20,8 +20,8 @@
     vm.showLoader = showLoader;
     vm.hideLoader = hideLoader;
 
-    var releaseMode = false;
-    var version = '1.1.7';
+    var releaseMode = true;
+    var version = '1.1.8';
 
     setFavicon();
 
